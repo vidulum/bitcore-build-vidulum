268485b0bee35a4ebf4b725d5297d06409b4dfc8
Pit <ceo@snowgem.org>
2018-02-28 09:53:20 +0700

    change name to snowgem

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function startGulp(name, opts) {
   var tests = ['test/**/*.js'];
   var alljs = files.concat(tests);
 
-  var buildPath = './node_modules/bitcore-build-zen/';
+  var buildPath = './node_modules/bitcore-build-snowgem/';
   var buildModulesPath = buildPath + 'node_modules/';
   var buildBinPath = buildPath + 'node_modules/.bin/';
 
@@ -102,9 +102,9 @@ function startGulp(name, opts) {
     var browserifyCommand;
 
     if (name !== 'lib') {
-      browserifyCommand = buildBinPath + 'browserify --require ./index.js:' + fullname + ' --external bitcore-lib-zen -o ' + fullname + '.js';
+      browserifyCommand = buildBinPath + 'browserify --require ./index.js:' + fullname + ' --external bitcore-lib-snowgem -o ' + fullname + '.js';
     } else {
-      browserifyCommand = buildBinPath + 'browserify --require ./index.js:bitcore-lib-zen -o bitcore-lib-zen.js';
+      browserifyCommand = buildBinPath + 'browserify --require ./index.js:bitcore-lib-snowgem -o bitcore-lib-snowgem.js';
     }
 
     gulp.task('browser:uncompressed', shell.task([
